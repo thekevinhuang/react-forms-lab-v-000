f795345bb59df9973084e623100ba18e1f0497e1
Kevin Huang <yellow.khaos@gmail.com>
2019-01-27 04:27:35 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -20,7 +20,7 @@ class TwitterMessage extends React.Component {
       <div>
         <strong>Your message:</strong>
         <input type="text" onChange={this.updateMessage} value={this.state.message} />
-        <p>{140-(this.state.message.length)}
+        <p>{140-(this.state.message.length)}</p>
       </div>
     );
   }
